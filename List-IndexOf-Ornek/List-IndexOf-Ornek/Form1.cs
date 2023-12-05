8d07b8db4e5eef0c8213254ed66ab2dca0817543
yazilimkodlama <145126274+yazilimkodlama@users.noreply.github.com>
2023-12-05 12:37:31 +0300

    Add files via upload

---
+++ List-IndexOf-Ornek/List-IndexOf-Ornek/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_IndexOf_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> personelListe = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            personelListe.Add("Ahmet");
+            personelListe.Add("Burak");
+            personelListe.Add("Ayşe");
+            personelListe.Add("Beyza");
+            personelListe.Add("Aliye");
+            personelListe.Add("Kemal");
+            personelListe.Add("Ruziye");
+            personelListe.Add("Handan");
+            personelListe.Add("Muzaffer");
+
+            listBox1.DataSource = personelListe.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aranan=textBox1.Text;   
+            int sira = personelListe.IndexOf(aranan);
+
+
+            if(sira >= 0)
+            {
+                listBox1.SelectedIndex = sira;
+                MessageBox.Show("Aradığınız personel " + (sira + 1) + ". sırada.");
+            }
+
+            else
+            {
+                MessageBox.Show("Personel bulunamadı.");
+            }
+           
+        }
+    }
+}
